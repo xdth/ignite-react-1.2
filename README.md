2e0ede9968f6f3e5f4e4544580d5b3429187d617
xdth <info@dthlabs.com>
2022-04-06 19:11:21 -0400

    Deleting MovieProps from App and adding readme

---
+++ README.md
@@ -0,0 +1,10 @@
+Rocketseat challenge, completed.
+
+Clone the repo:
+- git clone https://github.com/xdth/ignite-react-1.2.git
+
+Run with (both at the same time): 
+- yarn server
+- yarn dev
+
+<img src="https://i.imgur.com/LslRuYI.png" alt="screenshot">
--- src/App.tsx
+++ src/App.tsx
@@ -14,17 +14,6 @@ export interface GenreResponseProps {
   title: string;
 }
 
-interface MovieProps {
-  imdbID: string;
-  Title: string;
-  Poster: string;
-  Ratings: Array<{
-    Source: string;
-    Value: string;
-  }>;
-  Runtime: string;
-}
-
 export function App() {
   const [selectedGenreId, setSelectedGenreId] = useState(1);
   const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
